d11a711346c941fcb6720813fe635f48a0fcd399
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-01-08 20:37:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/databinding_android/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.databinding_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding_android.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState : Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this , R.layout.activity_main)
+        binding.tvLabel.text = "Introduction of Data Binding"
+    }
+}
