21bb3628999a08981cb5a1ffc63869f09864de6a
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-01-08 23:02:35 +0530

    Example of data binding version1
    In this example a simple example of data binding is given where activity and xml file are related.

--- app/src/main/java/com/example/databinding_android/MainActivity.kt
+++ app/src/main/java/com/example/databinding_android/MainActivity.kt
@@ -12,6 +12,6 @@ class MainActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this , R.layout.activity_main)
-        binding.tvLabel.text = "Introduction of Data Binding"
+        binding.quote = Quote("Hello World!!!", "Raaj Kanchan")
     }
 }
---
+++ app/src/main/java/com/example/databinding_android/Quote.kt
@@ -0,0 +1,3 @@
+package com.example.databinding_android
+
+data class Quote(val quote : String , val author : String)
